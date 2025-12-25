0db91ac319045e98e6f3837895bf7bc75f149a08
Anna Grygoryeva <annagrygoryeva3101@gmail.com>
2025-12-25 19:56:10 +0000

    Complete task

---
+++ index.js
@@ -0,0 +1,27 @@
+//  - температури повітря у Цельсіях (24);
+const airTemperatureCelsius = 24;
+console.log(airTemperatureCelsius);
+
+// - назви книжки (undefined);
+let bookTitle = undefined;
+console.log(bookTitle);
+// або:
+let anotherBookTitle;
+
+// - довжини (3) та ширини (4) прямокутника;
+const rectangleLength = 3;
+const rectangleWidth = 4;
+console.log(rectangleLength);
+console.log(rectangleWidth);
+
+// - площі прямокутника (використовуючи змінні з попереднього пункту);
+const rectangleArea = rectangleLength * rectangleWidth;
+console.log(rectangleArea);
+
+// boolean
+const isTriangle = false;
+console.log(isTriangle);
+
+// - елемент (точно відомо що елементу не має - самі подумайте яке це значення).
+const element = null;
+console.log(element);
